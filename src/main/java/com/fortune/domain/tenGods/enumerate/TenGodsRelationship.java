f9dc8ea99b39361a97a728a72e11634b5f7a17a7
limgahyun <limgh2002@gmail.com>
2025-03-22 03:08:04 +0900

    fix: rename tenGods enumerate

--- src/main/java/com/fortune/domain/tenGods/enumerate/TenGodsRelationship.java
+++ src/main/java/com/fortune/domain/tenGods/enumerate/TenGodsRelationship.java
@@ -6,12 +6,11 @@ import lombok.ToString;
 @Getter
 @ToString
 public enum TenGodsRelationship {
-    // todo : 영어명칭 통일
-    PARALLEL("비겁"),
+    BIGEOB("비겁"),
     SIGSANG("식상"),
-    WEALTH("재성"),
-    OFFICER("관성"),
-    RESOURCE("인성");
+    JAESEONG("재성"),
+    GWANSEONG("관성"),
+    INSEONG("인성");
 
     private final String description;
 
