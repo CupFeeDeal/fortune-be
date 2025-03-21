b60684f0f74995489b63830c017b629064628e69
limgahyun <limgh2002@gmail.com>
2025-03-22 02:24:26 +0900

    feat: yinYangFiveElements entity

--- src/main/java/com/fortune/domain/yinYangFiveElements/entity/YinYangFIveElements.java
+++ src/main/java/com/fortune/domain/yinYangFiveElements/entity/YinYangFIveElements.java
@@ -1,4 +1,29 @@
 package com.fortune.domain.yinYangFiveElements.entity;
 
+import com.fortune.domain.yinYangFiveElements.enumerate.FiveElements;
+import com.fortune.domain.yinYangFiveElements.enumerate.YinYang;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class YinYangFIveElements {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "yin_yang_five_elements_id")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private FiveElements fiveElement;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private YinYang yinYang;
+
+    @Column(nullable = false)
+    private String description;
 }
